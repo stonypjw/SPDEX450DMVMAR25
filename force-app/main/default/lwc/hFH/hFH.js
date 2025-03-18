e19e3260de8dd40d76b1c79a2bcc3d32f8097e6b
stonypjw <jeremy.white@stonyp.com>
2025-03-18 12:00:11 -0700

    new file:   force-app/main/default/lwc/ghiehwgieruow/__tests__/ghiehwgieruow.test.js 	new file:   force-app/main/default/lwc/ghiehwgieruow/ghiehwgieruow.html 	new file:   force-app/main/default/lwc/ghiehwgieruow/ghiehwgieruow.js 	new file:   force-app/main/default/lwc/ghiehwgieruow/ghiehwgieruow.js-meta.xml 	new file:   force-app/main/default/lwc/hFH/__tests__/hFH.test.js 	new file:   force-app/main/default/lwc/hFH/hFH.html 	new file:   force-app/main/default/lwc/hFH/hFH.js 	new file:   force-app/main/default/lwc/hFH/hFH.js-meta.xml 	modified:   force-app/main/default/lwc/helloWorldWrapper/helloWorldWrapper.html 	modified:   force-app/main/default/lwc/helloWorldWrapper/helloWorldWrapper.js

---
+++ force-app/main/default/lwc/hFH/hFH.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class HFH extends LightningElement {}
--- force-app/main/default/lwc/helloWorldWrapper/helloWorldWrapper.js
+++ force-app/main/default/lwc/helloWorldWrapper/helloWorldWrapper.js
@@ -2,4 +2,10 @@ import { LightningElement, api } from 'lwc';
 
 export default class HelloWorldWrapper extends LightningElement {
     @api appBuilderGreeting = 'World';
+    @api objectApiName;
+    @api recordId;
+
+    changeText(){
+        this.appBuilderGreeting = 'Changed'; 
+    }
 }
